62aca93b092fbbde77ba3e8e0a5514c747b85eb1
Jonathan Hurtt <JonHurtt@gmail.com>
2023-11-15 08:31:03 -0500

    updating terraform

--- terraform/build_apache_ec2.tf
+++ terraform/build_apache_ec2.tf
@@ -20,22 +20,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-/*
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.web-sg.id]
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y apache2
-              sed -i -e 's/80/8080/' /etc/apache2/ports.conf
-              echo "<p>Hello World!<br><br>this is part of the secure-sdlc created by Jonathan Hurtt (https://github.com/jonhurtt/secure-sdlc/)<p>" > /var/www/html/index.html
-              systemctl restart apache2
-              EOF
-}*/
-
 resource "aws_instance" "apache_ec2_instance" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
