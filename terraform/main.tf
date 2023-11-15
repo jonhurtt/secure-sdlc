f904f5ea0890b4d86c26f9b656d04edbe4dedb22
Jonathan Hurtt <JonHurtt@gmail.com>
2023-11-15 08:29:38 -0500

    updating terraform

--- terraform/main.tf
+++
@@ -1,6 +0,0 @@
-#https://developer.hashicorp.com/terraform/tutorials/kubernetes/eks
-#https://github.com/hashicorp/learn-terraform-provision-eks-cluster/blob/main/main.tf 
-
-data "aws_availability_zones" "available_zones" {
-  state = "available"
-}
