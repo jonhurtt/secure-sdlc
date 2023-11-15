5b2a5ca29b1e2cf16b5c27ee2bf914b693a33730
Jonathan Hurtt <JonHurtt@gmail.com>
2023-11-15 10:13:34 -0500

    fixing outputs

--- terraform/modules/aws-ec2-scanner/outputs.tf
+++ terraform/modules/aws-ec2-scanner/outputs.tf
@@ -1,4 +1,4 @@
 output "public_dns" {
   description = "IP Address of EC2 Scanner Instance"
-  value = "${aws_instance.apache_ec2_instance.public_dns}"
+  value = "${aws_instance.pc_scanner_instance.public_dns}"
 }
