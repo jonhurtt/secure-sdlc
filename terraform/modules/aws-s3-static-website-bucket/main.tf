f3143401c10771feab58ad76e5f705e6a8f107a0
Jonathan Hurtt <JonHurtt@gmail.com>
2023-11-16 13:21:29 -0500

    fixing s3 terraform

--- terraform/modules/aws-s3-static-website-bucket/main.tf
+++ terraform/modules/aws-s3-static-website-bucket/main.tf
@@ -24,6 +24,23 @@ resource "aws_s3_bucket_acl" "s3_bucket" {
   acl = "public-read"
 }
 
+resource "aws_s3_bucket_ownership_controls" "s3_bucket_acl_ownership" {
+  bucket = aws_s3_bucket.s3_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+  depends_on = [aws_s3_bucket_public_access_block.example]
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_policy" "s3_bucket" {
   bucket = aws_s3_bucket.s3_bucket.id
 
